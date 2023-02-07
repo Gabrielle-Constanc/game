38599aa8dd050aca073a0c602e9d271521bdbe31
Gabrielle-Constanc <113620328+Gabrielle-Constanc@users.noreply.github.com>
2023-02-07 12:31:40 -0500

    Add files via upload

---
+++ pygame/click_game.py
@@ -0,0 +1,12 @@
+my_blinky = Actor('character')
+my_blinky.pos = 100,50
+
+WIDTH =500
+HEIGHT = my_blinky.height + 20
+
+def draw():
+    screen.clear()
+    screen.fill((255,255,255))
+    my_character.draw()
+
+def update# Write your code here :-)
